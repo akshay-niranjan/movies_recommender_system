ce390c3f6811d34d18e0002385ae71b83e3c9002
Akshay Niranjan <akshayniranjan51@gmail.com>
2025-10-01 23:36:33 +0530

    Update movies_recommender_app.py

--- movies_recommender_app.py
+++ movies_recommender_app.py
@@ -4,7 +4,7 @@ import joblib
 import requests
 
 movies = pickle.load(open('movies.pkl','rb'))
-similarity = joblib.load("data.pkl")
+similarity = joblib.load("similarity2.pkl")
 
 movies_title = movies['title'].values
 
@@ -60,6 +60,7 @@ if st.button('Recommend'):
         st.write(names[4])
         st.image(posters[4])
     
+
 
 
 
