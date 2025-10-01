c9ad3ea5445a3dd2778f42d2553c59930557cfc3
Akshay Niranjan <akshayniranjan51@gmail.com>
2025-10-01 23:03:45 +0530

    Update movies_recommender_app.py

--- movies_recommender_app.py
+++ movies_recommender_app.py
@@ -2,15 +2,15 @@ import streamlit as st
 import pickle
 import requests
 
-movies = pickle.load(open('movies.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
+movies = pickle.load(open('Project_Movie_Recommender/movies.pkl','rb'))
+similarity = pickle.load(open('Project_Movie_Recommender/similarity.pkl','rb'))
 
 movies_title = movies['title'].values
 
 def fetch_posters(movie_id):
-     response=requests.get(url='https://api.themoviedb.org/3/movie/{}?api_key=d0dafc4b86d078ed79f7377df1982d34'.format(movie_id))
-     data=response.json()
-     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+    response=requests.get(url='https://api.themoviedb.org/3/movie/{}?api_key=d0dafc4b86d078ed79f7377df1982d34'.format(movie_id))
+    data=response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 def recommend(movie):
     movie_index = movies[movies.title == movie].index[0]
@@ -18,8 +18,11 @@ def recommend(movie):
     movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
 
     recommended_movies=[]
+    similarity_score=[]
     recommended_movies_posters=[]
     for i in movies_list:
+        similarity_score.append((i[1]*100+50).round(2))
+
         movie_id = movies.iloc[i[0]].movie_id
 
         recommended_movies.append(movies.iloc[i[0]].title)
@@ -27,7 +30,7 @@ def recommend(movie):
         #fetch poster through api
         recommended_movies_posters.append(fetch_posters(movie_id))
 
-    return recommended_movies,recommended_movies_posters
+    return recommended_movies,similarity_score,recommended_movies_posters
 
 st.title('Movie Recommender System')
 
@@ -37,27 +40,27 @@ selected_movie_name = st.selectbox(
 )
 
 if st.button('Recommend'):
-    names = recommend(selected_movie_name)
-    for i in recommendations:
-        st.write(i)
-
+    names,simi,posters = recommend(selected_movie_name)
+    
     col1,col2,col3,col4,col5=st.columns(5)
     with col1:
-        st.header(names[0])
+        st.write(names[0])
         st.image(posters[0])
     with col2:
-        st.header(names[1])
+        st.write(names[1])
         st.image(posters[1])
     with col3:
-        st.header(names[2])
+        st.write(names[2])
         st.image(posters[2])
     with col4:
-        st.header(names[3])
+        st.write(names[3])
         st.image(posters[3])
     with col5:
-        st.header(names[4])
+        st.write(names[4])
         st.image(posters[4])
     
+
+
 
 
 
