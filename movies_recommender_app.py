01787247812032d12dd8cd79f5ddf6bb56222cba
Akshay Niranjan <akshayniranjan51@gmail.com>
2025-10-01 22:39:19 +0530

    Update movies_recommender_app.py

--- movies_recommender_app.py
+++ movies_recommender_app.py
@@ -7,10 +7,10 @@ similarity = pickle.load(open('similarity.pkl','rb'))
 
 movies_title = movies['title'].values
 
-# def fetch_posters(movie_id):
-#     response=requests.get(url='https://api.themoviedb.org/3/movie/{}?api_key=d0dafc4b86d078ed79f7377df1982d34'.format(movie_id))
-#     data=response.json()
-#     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+def fetch_posters(movie_id):
+     response=requests.get(url='https://api.themoviedb.org/3/movie/{}?api_key=d0dafc4b86d078ed79f7377df1982d34'.format(movie_id))
+     data=response.json()
+     return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
 
 def recommend(movie):
     movie_index = movies[movies.title == movie].index[0]
@@ -18,16 +18,16 @@ def recommend(movie):
     movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
 
     recommended_movies=[]
-    #recommended_movies_posters=[]
+    recommended_movies_posters=[]
     for i in movies_list:
         movie_id = movies.iloc[i[0]].movie_id
 
         recommended_movies.append(movies.iloc[i[0]].title)
 
-        #fetch poster through api
+        fetch poster through api
         recommended_movies_posters.append(fetch_posters(movie_id))
 
-    return recommended_movies#,recommended_movies_posters
+    return recommended_movies,recommended_movies_posters
 
 st.title('Movie Recommender System')
 
@@ -56,7 +56,8 @@ if st.button('Recommend'):
         st.image(posters[3])
     with col5:
         st.header(names[4])
-       # st.image(posters[4])
+        st.image(posters[4])
     
+
 
 
